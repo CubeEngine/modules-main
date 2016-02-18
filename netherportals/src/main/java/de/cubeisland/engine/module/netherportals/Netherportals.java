3add59708b8626dd8f629c928f2889da8a5680b7
Anselm Brehme <Faithcaio@cubeisland.de>
2016-02-18 21:44:50 +0100

    nether-portals module

---
+++ netherportals/src/main/java/de/cubeisland/engine/module/netherportals/Netherportals.java
@@ -0,0 +1,55 @@
+package de.cubeisland.engine.module.netherportals;
+
+import de.cubeisland.engine.modularity.asm.marker.ModuleInfo;
+import de.cubeisland.engine.modularity.core.Module;
+import de.cubeisland.engine.modularity.core.marker.Enable;
+import de.cubeisland.engine.module.netherportals.NetherportalsConfig.WorldSection;
+import org.cubeengine.service.filesystem.ModuleConfig;
+import org.cubeengine.service.world.ConfigWorld;
+import org.spongepowered.api.entity.Transform;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.cause.entity.teleport.TeleportCause;
+import org.spongepowered.api.event.cause.entity.teleport.TeleportType;
+import org.spongepowered.api.event.cause.entity.teleport.TeleportTypes;
+import org.spongepowered.api.event.entity.DisplaceEntityEvent;
+import org.spongepowered.api.event.filter.cause.First;
+import org.spongepowered.api.world.World;
+
+@ModuleInfo(name = "Netherportals", description = "Modifies Vanilla Portal behaviours")
+public class Netherportals extends Module
+{
+    @ModuleConfig private NetherportalsConfig config;
+
+    @Listener
+    public void onPortal(DisplaceEntityEvent.Teleport event, @First TeleportCause cause)
+    {
+        WorldSection section = config.worldSettings.get(new ConfigWorld(event.getFromTransform().getExtent()));
+        if (section != null && section.enablePortalRouting)
+        {
+            Transform<World> to = event.getToTransform();
+            TeleportType type = cause.getTeleportType();
+            if (type == TeleportTypes.NETHER_PORTAL)
+            {
+                if (section.netherTarget != null)
+                {
+                    to = to.setExtent(section.netherTarget.getWorld());
+                    // TODO netherPortalScale
+
+                    event.setToTransform(to);
+                    // TODO PortalCreation?
+                }
+            }
+            else if (type == TeleportTypes.END_PORTAL)
+            {
+                if (section.endTarget != null)
+                {
+                    to = to.setExtent(section.endTarget.getWorld());
+                    // TODO endPortalTargetLocation?
+
+                    event.setToTransform(to);
+                    // TODO cancel PortalCreation if not in end?
+                }
+            }
+        }
+    }
+}
---
+++ netherportals/src/main/java/de/cubeisland/engine/module/netherportals/NetherportalsConfig.java
@@ -0,0 +1,38 @@
+package de.cubeisland.engine.module.netherportals;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import de.cubeisland.engine.reflect.Section;
+import de.cubeisland.engine.reflect.annotations.Comment;
+import de.cubeisland.engine.reflect.codec.yaml.ReflectedYaml;
+import org.cubeengine.service.world.ConfigWorld;
+import org.spongepowered.api.Sponge;
+
+@SuppressWarnings("all")
+public class NetherportalsConfig extends ReflectedYaml
+{
+    @Comment("Portal behaviour setting per world")
+    public Map<ConfigWorld, WorldSection> worldSettings = new HashMap<>();
+
+    public static class WorldSection implements Section
+    {
+        @Comment("If enabled vanilla behaviour will be replaced")
+        public boolean enablePortalRouting = false;
+
+        @Comment("Sets the target of netherportals to this world")
+        public ConfigWorld netherTarget;
+        @Comment("Sets the scale of netherportals to the other world\n"
+            + "For default vanilla scale leave this value empty")
+        public Integer netherTargetScale;
+
+        @Comment("Sets the target of enderportals to this world")
+        public ConfigWorld endTarget;
+    }
+
+    @Override
+    public void onLoad()
+    {
+        worldSettings.put(new ConfigWorld(Sponge.getServer().getDefaultWorldName()), new WorldSection());
+    }
+}
