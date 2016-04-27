dc989c9d4d83c580e1040d016ff0496c83a0f96d
Anselm Brehme <Faithcaio@cubeisland.de>
2016-04-27 22:35:01 +0200

    .

--- conomy/src/main/java/org/cubeengine/module/conomy/ConfigCurrency.java
+++ conomy/src/main/java/org/cubeengine/module/conomy/ConfigCurrency.java
@@ -25,7 +25,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-import org.cubeengine.service.ContextUtil;
 import org.spongepowered.api.service.context.Context;
 import org.spongepowered.api.service.economy.Currency;
 import org.spongepowered.api.service.economy.account.UniqueAccount;
