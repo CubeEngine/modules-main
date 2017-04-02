1c7ea0f3e0966aa8b66af516b6e136a0cb60b788
Anselm Brehme <Faithcaio@cubeisland.de>
2017-04-02 12:26:06 +0200

    region list and info command

--- protector/src/main/java/org/cubeengine/module/protector/command/RegionCommands.java
+++ protector/src/main/java/org/cubeengine/module/protector/command/RegionCommands.java
@@ -17,24 +17,44 @@
  */
 package org.cubeengine.module.protector.command;
 
+import static java.util.stream.Collectors.toList;
 import static org.cubeengine.libcube.service.i18n.formatter.MessageType.NEGATIVE;
+import static org.cubeengine.libcube.service.i18n.formatter.MessageType.NEUTRAL;
 import static org.cubeengine.libcube.service.i18n.formatter.MessageType.POSITIVE;
+import static org.spongepowered.api.text.format.TextColors.GOLD;
+import static org.spongepowered.api.text.format.TextColors.WHITE;
+import static org.spongepowered.api.text.format.TextColors.YELLOW;
 
 import org.cubeengine.butler.parametric.Command;
 import org.cubeengine.butler.parametric.Default;
+import org.cubeengine.butler.parametric.Flag;
 import org.cubeengine.butler.parametric.Optional;
 import org.cubeengine.libcube.service.Selector;
 import org.cubeengine.libcube.service.command.CommandManager;
 import org.cubeengine.libcube.service.command.ContainerCommand;
 import org.cubeengine.libcube.service.i18n.I18n;
+import org.cubeengine.libcube.util.math.shape.Cuboid;
 import org.cubeengine.libcube.util.math.shape.Shape;
 import org.cubeengine.module.protector.Protector;
 import org.cubeengine.module.protector.RegionManager;
 import org.cubeengine.module.protector.region.Region;
+import org.cubeengine.module.protector.region.RegionConfig;
+import org.spongepowered.api.CatalogType;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.event.filter.cause.Named;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.format.TextColors;
+import org.spongepowered.api.util.Tristate;
+import org.spongepowered.api.world.Locatable;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 @Command(name = "region", desc = "Manages the regions")
 public class RegionCommands extends ContainerCommand
@@ -102,8 +122,42 @@ public class RegionCommands extends ContainerCommand
         i18n.sendTranslated(context, POSITIVE, "Region {name} selected!", region.getName());
     }
 
-    public void list(CommandSource context, @Optional String match)
+    @Command(desc = "Lists regions")
+    public void list(CommandSource context, @Optional String match, @Named("in") World world)
     {
+        if (world == null && context instanceof Locatable)
+        {
+            world = ((Locatable) context).getWorld();
+        }
+        Map<UUID, Map<String, Region>> regions = manager.getRegions();
+        List<Region> list = new ArrayList<>();
+        if (world == null)
+        {
+            for (Map<String, Region> map : regions.values())
+            {
+                list.addAll(map.values());
+            }
+        }
+        else
+        {
+            list.addAll(regions.getOrDefault(world.getUniqueId(), Collections.emptyMap()).values());
+        }
+
+        if (match != null)
+        {
+            list = list.stream().filter(r -> r.getName().matches(match) || r.getName().startsWith(match)).collect(toList());
+        }
+
+        if (list.isEmpty())
+        {
+            i18n.sendTranslated(context, NEGATIVE, "No Regions found");
+            return;
+        }
+        i18n.sendTranslated(context, NEUTRAL, "The following regions were found:");
+        for (Region region : list)
+        {
+            context.sendMessage(Text.of(" - ", GOLD, region.getName(), WHITE, " (", GOLD, region.getWorld().getName(), WHITE, ")"));
+        }
 
     }
 
@@ -112,14 +166,113 @@ public class RegionCommands extends ContainerCommand
 
     }
 
-    public void info(CommandSource context, @Default Region region)
+    @Command(desc = "Displays Region info")
+    public void info(CommandSource context, @Default Region region, @Flag boolean allSettings)
     {
+        i18n.sendTranslated(context, POSITIVE, "Region {name} in {world}", region.getName(), region.getWorld());
 
+        Cuboid cuboid = region.getCuboid();
+        // TODO tp on click to center
+        i18n.sendTranslated(context, POSITIVE, "Inside Cuboid of {vector} to {vector}", cuboid.getMinimumPoint(), cuboid.getMaximumPoint());
+
+        // TODO priority
+        i18n.sendTranslated(context, POSITIVE, "Settings:");
+        RegionConfig.Settings settings = region.getSettings();
+        showSetting(context, i18n.translate(context, "build"), settings.build, allSettings);
+        showSetting(context, i18n.translate(context, "move"), settings.move, allSettings);
+        showSetting(context, i18n.translate(context, "use-block"), settings.use.block, allSettings);
+        showSetting(context, i18n.translate(context, "use-item"), settings.use.item, allSettings);
+        showSetting(context, i18n.translate(context, "useall blocks"), settings.use.all.block, allSettings);
+        showSetting(context, i18n.translate(context, "useall items"), settings.use.all.item, allSettings);
+        showSetting(context, i18n.translate(context, "useall container"), settings.use.all.container, allSettings);
+        showSetting(context, i18n.translate(context, "useall openables"), settings.use.all.open, allSettings);
+        showSetting(context, i18n.translate(context, "useall redstone"), settings.use.all.redstone, allSettings);
+
+        showSetting(context, i18n.translate(context, "spawn naturally"), settings.spawn.naturally, allSettings);
+        showSetting(context, i18n.translate(context, "spawn player"), settings.spawn.player, allSettings);
+        showSetting(context, i18n.translate(context, "spawn plugin"), settings.spawn.plugin, allSettings);
+
+        showSetting(context, i18n.translate(context, "blockdamage all explosion"), settings.blockDamage.allExplosion, allSettings);
+        showSetting(context, i18n.translate(context, "blockdamage player explosion"), settings.blockDamage.playerExplosion, allSettings);
+        showSetting(context, i18n.translate(context, "blockdamage block"), settings.blockDamage.block, allSettings);
+        showSetting(context, i18n.translate(context, "blockdamage monster"), settings.blockDamage.monster, allSettings);
+
+        showSetting(context, i18n.translate(context, "blocked commands"), settings.blockedCommands, allSettings);
+
+        showSetting(context, i18n.translate(context, "deadcircuit"), settings.deadCircuit, allSettings);
+
+        // TODO remaining settings
     }
 
-    public void show(CommandSource context)
+    private void showSetting(CommandSource cs, String name, Tristate value, boolean allSettings)
     {
+        if (value != Tristate.UNDEFINED || allSettings)
+        {
+            cs.sendMessage(Text.of(YELLOW, name, ": ", GOLD, toText(cs, value)));
+        }
+    }
 
+    private void showSetting(CommandSource cs, String name, Map<?, Tristate> values, boolean allSettings)
+    {
+        int pos = 0;
+        int neg = 0;
+        for (Tristate tristate : values.values())
+        {
+            if (tristate == Tristate.TRUE)
+            {
+                pos++;
+            }
+            else if (tristate == Tristate.FALSE)
+            {
+                neg++;
+            }
+        }
+
+        if (values.size() > 0 || allSettings)
+        {
+            Text trueText = Text.of(YELLOW, i18n.translate(cs, "Allowed"), " ", GOLD, pos);
+            Text falseText = Text.of(YELLOW, i18n.translate(cs, "Denied"), " ", GOLD, neg);
+            cs.sendMessage(Text.of(YELLOW, name, ": ", trueText, " ", falseText));
+            for (Map.Entry<?, Tristate> entry : values.entrySet())
+            {
+                String key;
+                if (entry.getKey() instanceof Enum)
+                {
+                    key = ((Enum) entry.getKey()).name();
+                }
+                else if (entry.getKey() instanceof CatalogType)
+                {
+                    key = ((CatalogType) entry.getKey()).getName();
+                }
+                else if (entry.getKey() instanceof String)
+                {
+                    key = ((String) entry.getKey());
+                }
+                else
+                {
+                    throw new IllegalArgumentException("Unsupported KeyType for map: " + entry.getKey().getClass().getSimpleName());
+                }
+                cs.sendMessage(Text.of(" - ", YELLOW, key, ": ", GOLD, toText(cs, entry.getValue())));
+            }
+        }
+    }
+
+    private Text toText(CommandSource cs, Tristate val)
+    {
+        switch (val)
+        {
+            case TRUE:
+                return Text.of(TextColors.DARK_GREEN, i18n.translate(cs,"Allowed"));
+            case FALSE:
+                return Text.of(TextColors.DARK_RED, i18n.translate(cs,"Denied"));
+            default:
+                return Text.of(TextColors.GOLD, i18n.translate(cs,"Undefined"));
+        }
+    }
+
+    public void show(Player context)
+    {
+        // Show boundry with player-only blocks
     }
 
     public void parent(CommandSource context, Region parent, @Default Region region)
@@ -134,6 +287,6 @@ public class RegionCommands extends ContainerCommand
 
     public void redstonedefine(CommandSource context, String name)
     {
-
+        // Rightclick redstone - define region around the whole circuit
     }
 }
