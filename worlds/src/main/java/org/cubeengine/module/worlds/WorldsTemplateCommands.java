0cb4f82b497cbad80650b2e41cba9d7b41e47ef2
Anselm Brehme <development.anselm@breh.me>
2023-01-14 13:41:02 +0100

    API10 fix world template commands

--- worlds/src/main/java/org/cubeengine/module/worlds/WorldsTemplateCommands.java
+++ worlds/src/main/java/org/cubeengine/module/worlds/WorldsTemplateCommands.java
@@ -17,9 +17,7 @@
  */
 package org.cubeengine.module.worlds;
 
-import static org.cubeengine.libcube.service.i18n.formatter.MessageType.*;
-import static org.cubeengine.libcube.service.i18n.formatter.MessageType.NEUTRAL;
-
+import java.util.Random;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import net.kyori.adventure.text.Component;
@@ -34,6 +32,7 @@ import org.cubeengine.libcube.util.ComponentUtil;
 import org.spongepowered.api.ResourceKey;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.command.CommandCause;
+import org.spongepowered.api.data.Keys;
 import org.spongepowered.api.entity.living.player.gamemode.GameMode;
 import org.spongepowered.api.entity.living.player.gamemode.GameModes;
 import org.spongepowered.api.registry.RegistryTypes;
@@ -48,7 +47,11 @@ import org.spongepowered.api.world.server.WorldManager;
 import org.spongepowered.api.world.server.WorldTemplate;
 import org.spongepowered.api.world.server.WorldTemplate.Builder;
 import org.spongepowered.math.vector.Vector3i;
-import java.util.Random;
+
+import static org.cubeengine.libcube.service.i18n.formatter.MessageType.CRITICAL;
+import static org.cubeengine.libcube.service.i18n.formatter.MessageType.NEGATIVE;
+import static org.cubeengine.libcube.service.i18n.formatter.MessageType.NEUTRAL;
+import static org.cubeengine.libcube.service.i18n.formatter.MessageType.POSITIVE;
 
 @Singleton
 @Command(name = "template", desc = "Worlds modify template commands")
@@ -78,13 +81,13 @@ public class WorldsTemplateCommands extends DispatcherCommand
             return;
         }
 
-        final WorldGenerationConfig wgenCfg = WorldGenerationConfig.Mutable.builder().seed(new Random().nextLong())
-                                                                           .generateFeatures(true).generateBonusChest(false).build();
+        final WorldGenerationConfig wgenCfg = WorldGenerationConfig.builder().seed(new Random().nextLong())
+                                                                           .generateStructures(true).generateBonusChest(false).build();
         final Builder builder = WorldTemplate.builder();
         if (displayName != null)
         {
             final Component display = ComponentUtil.fromLegacy(displayName);
-           builder.displayName(display);
+           builder.add(Keys.DISPLAY_NAME, display);
         }
 
         ChunkGenerator generator = ChunkGenerator.overworld();
@@ -98,22 +101,22 @@ public class WorldsTemplateCommands extends DispatcherCommand
         }
 
         final WorldTemplate template = builder
-                        .worldType(type.asDefaultedReference(RegistryTypes.WORLD_TYPE))
-                        .generator(generator)
-                        .generationConfig(wgenCfg)
-                        .gameMode(GameModes.SURVIVAL)
-                        .difficulty(Difficulties.NORMAL)
-                        .serializationBehavior(SerializationBehavior.AUTOMATIC)
-                        .loadOnStartup(loadOnStartup)
-                        .performsSpawnLogic(true)
-                        .hardcore(false)
-                        .commands(true)
-                        .pvp(true)
+                        .add(Keys.WORLD_TYPE, type)
+                        .add(Keys.CHUNK_GENERATOR, generator)
+                        .add(Keys.WORLD_GEN_CONFIG, wgenCfg)
+                        .add(Keys.GAME_MODE, GameModes.SURVIVAL.get())
+                        .add(Keys.WORLD_DIFFICULTY, Difficulties.NORMAL.get())
+                        .add(Keys.SERIALIZATION_BEHAVIOR, SerializationBehavior.AUTOMATIC)
+                        .add(Keys.IS_LOAD_ON_STARTUP, loadOnStartup)
+                        .add(Keys.PERFORM_SPAWN_LOGIC, true)
+                        .add(Keys.HARDCORE, false)
+                        .add(Keys.COMMANDS, true)
+                        .add(Keys.PVP, true)
 //                .viewDistance(distance)
 //                .spawnPosition(pos)
                         .key(worldKey)
                         .build();
-        wm.saveTemplate(template).whenComplete((b, t) -> {
+        Sponge.server().dataPackManager().save(template).whenComplete((b, t) -> {
             i18n.send(context, POSITIVE, "World Template {name#key} {txt#display} created!", worldKey.asString(), displayName == null ? worldKey.asString() : displayName);
             i18n.send(context, NEUTRAL, "Use {name#command} commands to further modify your world template", "/worlds template");
             i18n.send(context, NEUTRAL, "Use {name#command} to create the world and load it", "/worlds create " + worldKey.asString());
@@ -124,107 +127,88 @@ public class WorldsTemplateCommands extends DispatcherCommand
     @Command(desc = "Sets world spawn")
     public void spawn(CommandCause context, WorldTemplate world, Vector3i spawnPoint)
     {
-        final WorldTemplate template = world.asBuilder().spawnPosition(spawnPoint).build();
-        Sponge.server().worldManager().saveTemplate(template);
+        final WorldTemplate template = world.asBuilder().add(Keys.SPAWN_POSITION, spawnPoint).build();
+        Sponge.server().dataPackManager().save(template);
         i18n.send(context, POSITIVE, "Template {name} world spawn changed to {vector}", world.key().asString(), spawnPoint);
     }
 
     @Command(desc = "Sets view distance")
     public void viewDistance(CommandCause context, WorldTemplate world, int viewDistance)
     {
-        final WorldTemplate template = world.asBuilder().viewDistance(viewDistance).build();
-        Sponge.server().worldManager().saveTemplate(template);
+        final WorldTemplate template = world.asBuilder().add(Keys.VIEW_DISTANCE, viewDistance).build();
+        Sponge.server().dataPackManager().save(template);
         i18n.send(context, POSITIVE, "Template {name} view distance changed to {number}", world.key().asString(), viewDistance);
-    }
-
-    @Command(desc = "Sets generate features")
-    public void features(CommandCause context, WorldTemplate world, boolean generateFeatures)
-    {
-        WorldGenerationConfig cfg = WorldGenerationConfig.Mutable.builder().from(world.generationConfig()).generateFeatures(generateFeatures).build();
-        final WorldTemplate template = world.asBuilder().generationConfig(cfg).build();
-        Sponge.server().worldManager().saveTemplate(template);
-        i18n.send(context, POSITIVE, "Template {name} generate features changed to {name}", world.key().asString(), String.valueOf(generateFeatures));
-    }
-
-    @Command(desc = "Sets spawn bonus chest")
-    public void bonusChest(CommandCause context, WorldTemplate world, boolean spawnChest)
-    {
-        WorldGenerationConfig cfg = WorldGenerationConfig.Mutable.builder().from(world.generationConfig()).generateBonusChest(spawnChest).build();
-        final WorldTemplate template = world.asBuilder().generationConfig(cfg).build();
-        Sponge.server().worldManager().saveTemplate(template);
-        i18n.send(context, POSITIVE, "Template {name} bonus chest changed to {name}", world.key().asString(), String.valueOf(spawnChest));
     }
 
     @Command(desc = "Sets load on startup")
     public void autoload(CommandCause context, WorldTemplate world, boolean loadOnStartup)
     {
-        final WorldTemplate template = world.asBuilder().loadOnStartup(loadOnStartup).build();
-        Sponge.server().worldManager().saveTemplate(template);
+        final WorldTemplate template = world.asBuilder().add(Keys.IS_LOAD_ON_STARTUP, loadOnStartup).build();
+        Sponge.server().dataPackManager().save(template);
         i18n.send(context, POSITIVE, "Template {name} load on startup changed to {name}", world.key().asString(), String.valueOf(loadOnStartup));
     }
 
     @Command(desc = "Sets spawnchunks loaded")
     public void spawnChunks(CommandCause context, WorldTemplate world, boolean loaded)
     {
-        final WorldTemplate template = world.asBuilder().performsSpawnLogic(loaded).build();
-        Sponge.server().worldManager().saveTemplate(template);
+        final WorldTemplate template = world.asBuilder().add(Keys.PERFORM_SPAWN_LOGIC, loaded).build();
+        Sponge.server().dataPackManager().save(template);
         i18n.send(context, POSITIVE, "Template {name} spawn-chunks loaded changed to {name}", world.key().asString(), String.valueOf(loaded));
     }
 
     @Command(desc = "Sets hardcore mode")
     public void hardcore(CommandCause context, WorldTemplate world, boolean hardcore)
     {
-        final WorldTemplate template = world.asBuilder().hardcore(hardcore).build();
-        Sponge.server().worldManager().saveTemplate(template);
+        final WorldTemplate template = world.asBuilder().add(Keys.HARDCORE, hardcore).build();
+        Sponge.server().dataPackManager().save(template);
         i18n.send(context, POSITIVE, "Template {name} hardcore mode changed to {name}", world.key().asString(), String.valueOf(hardcore));
     }
 
     @Command(desc = "Sets command usage")
     public void commands(CommandCause context, WorldTemplate world, boolean allowed)
     {
-        final WorldTemplate template = world.asBuilder().commands(allowed).build();
-        Sponge.server().worldManager().saveTemplate(template);
+        final WorldTemplate template = world.asBuilder().add(Keys.COMMANDS, allowed).build();
+        Sponge.server().dataPackManager().save(template);
         i18n.send(context, POSITIVE, "Template {name} command usage changed to {name}", world.key().asString(), String.valueOf(allowed));
     }
 
     @Command(desc = "Sets pvp")
     public void pvp(CommandCause context, WorldTemplate world, boolean pvp)
     {
-        final WorldTemplate template = world.asBuilder().pvp(pvp).build();
-        Sponge.server().worldManager().saveTemplate(template);
+        final WorldTemplate template = world.asBuilder().add(Keys.PVP, pvp).build();
+        Sponge.server().dataPackManager().save(template);
         i18n.send(context, POSITIVE, "Template {name} pvp changed to {name}", world.key().asString(), String.valueOf(pvp));
     }
 
     @Command(desc = "Sets the seed")
     public void seed(CommandCause context, WorldTemplate world, String seed)
     {
-        WorldGenerationConfig cfg = WorldGenerationConfig.Mutable.builder().from(world.generationConfig()).seed(seed.hashCode()).build();
-        final WorldTemplate template = world.asBuilder().generationConfig(cfg).build();
-        Sponge.server().worldManager().saveTemplate(template);
+        final WorldTemplate template = world.asBuilder().add(Keys.SEED, (long) seed.hashCode()).build();
+        Sponge.server().dataPackManager().save(template);
         i18n.send(context, POSITIVE, "Template {name} seed changed to {name}", world.key().asString(), seed);
     }
 
     @Command(desc = "Sets the serialization behavior")
     public void serialize(CommandCause context, WorldTemplate world, SerializationBehavior serializationBehavior)
     {
-        final WorldTemplate template = world.asBuilder().serializationBehavior(serializationBehavior).build();
-        Sponge.server().worldManager().saveTemplate(template);
+        final WorldTemplate template = world.asBuilder().add(Keys.SERIALIZATION_BEHAVIOR, serializationBehavior).build();
+        Sponge.server().dataPackManager().save(template);
         i18n.send(context, POSITIVE, "Template {name} serialization behavior changed to {name}", world.key().asString(), serializationBehavior.name());
     }
 
     @Command(desc = "Sets the difficulty")
     public void difficulty(CommandCause context, WorldTemplate world, Difficulty difficulty)
     {
-        final WorldTemplate template = world.asBuilder().difficulty(difficulty.asDefaultedReference(RegistryTypes.DIFFICULTY)).build();
-        Sponge.server().worldManager().saveTemplate(template);
+        final WorldTemplate template = world.asBuilder().add(Keys.WORLD_DIFFICULTY, difficulty).build();
+        Sponge.server().dataPackManager().save(template);
         i18n.send(context, POSITIVE, "Template {name} difficulty changed to {name}", world.key().asString(), difficulty.key(RegistryTypes.DIFFICULTY).asString());
     }
 
     @Command(desc = "Sets the default gamemode")
     public void gamemode(CommandCause context, WorldTemplate world, GameMode gameMode)
     {
-        final WorldTemplate template = world.asBuilder().gameMode(gameMode.asDefaultedReference(RegistryTypes.GAME_MODE)).build();
-        Sponge.server().worldManager().saveTemplate(template);
+        final WorldTemplate template = world.asBuilder().add(Keys.GAME_MODE, gameMode).build();
+        Sponge.server().dataPackManager().save(template);
         i18n.send(context, POSITIVE, "Template {name} gamemode changed to {name}", world.key().asString(), gameMode.key(RegistryTypes.GAME_MODE).asString());
     }
 
@@ -240,9 +224,9 @@ public class WorldsTemplateCommands extends DispatcherCommand
         {
             generator = ChunkGenerator.theEnd();
         }
-        final WorldTemplate template = world.asBuilder().worldType(type.asDefaultedReference(RegistryTypes.WORLD_TYPE)).generator(generator).build();
-        Sponge.server().worldManager().saveTemplate(template);
-        i18n.send(context, POSITIVE, "Template {name} worldtype changed to {name}", world.key().asString(), template.worldType().location().asString());
+        final WorldTemplate template = world.asBuilder().add(Keys.WORLD_TYPE, type).add(Keys.CHUNK_GENERATOR, generator).build();
+        Sponge.server().dataPackManager().save(template);
+        i18n.send(context, POSITIVE, "Template {name} worldtype changed to {name}", world.key().asString(), template.worldType().key(RegistryTypes.WORLD_TYPE).asString());
         if (chunkGenerator != null)
         {
             i18n.send(context, CRITICAL, "Custom ChunkGenerators not available yet!");
